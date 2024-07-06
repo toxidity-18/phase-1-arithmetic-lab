8d277810e17ab390ceabf45a9350b0b15b3ea042
Samuel <samuel.kamawira@student.moringaschool.com>
2024-07-06 07:03:02 +0300

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1=2;
+let num2=31;
+let multiply=num1*num2;
+let random =Math.floor(Math.random()*10)+1;
+let num3=16;
+let num4=6;
+let mod=16%4;
+let max=Math.max(15,12,9,5,20,19,);
